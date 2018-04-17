87d916caeca66a39c8444c09333684e615966107
egoleo <egoleo@gmail.com>
2018-04-17 09:26:12 +0000

    Initial commit

---
+++ lib/ubaba.ex
@@ -0,0 +1,18 @@
+defmodule Ubaba do
+  @moduledoc """
+  Documentation for Ubaba.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ubaba.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ubaba_test.exs
@@ -0,0 +1,8 @@
+defmodule UbabaTest do
+  use ExUnit.Case
+  doctest Ubaba
+
+  test "greets the world" do
+    assert Ubaba.hello() == :world
+  end
+end
